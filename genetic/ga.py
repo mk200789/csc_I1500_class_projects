6491fe64f469c2e0351480d22700d07ac6304c65
Wan Kim Mok <cafe.mui@gmail.com>
2015-12-05 00:03:33 -0500

    variables for  linear congruential random number generator

---
+++ genetic/ga.py
@@ -0,0 +1,8 @@
+#Xn+1 = (a*n + c) mod m.
+
+# m and c are coprime
+# a - 1 is divisible by all prime factors of m
+# a - 1 is divisible by 4 if m is divisble by 4
+m = 10000
+a = 3201
+c = 1013904223
